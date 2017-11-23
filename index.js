ad3b3545c354677dd2a610a80b83bd55c54dc257
Sastry Akella <sastry@akella.com>
2017-11-22 20:34:41 -0500

    Refactored to move routes to separate file

--- index.js
+++ index.js
@@ -8,45 +8,9 @@ server.connection({
     port: 8000
 });
 
-server.route([{
-    path: '/api/recipes',
-    method: 'GET',
-    handler: (request, reply) => {
+server.bind({db: db})
 
-        let sql = 'select * from recipes';
-        let params = [];
-
-        if(request.query.cuisine) {
-            sql += ' where cuisine = ?';
-            params.push(request.query.cuisine);
-        }
-        db.all(sql, params, (err, results) => {
-            if(err) throw err;
-
-            reply(results);
-        });
-    }
-}, {
-    path: '/api/recipes/{id}',
-    method: 'GET',
-    handler: (request, reply) => {
-
-        let sql = 'select * from recipes where id = ?';
- 
-        db.get(sql, [request.params.id], (err, results) => {
-            if(err) throw err;
-
-            console.log(results !== 'undefined')
-            if(typeof results !== 'undefined') {
-               return reply(results);
-            } 
-                
-            return reply('No cuisine found for ' + request.params.id).code(404);
-        });
-    }
-}
-
-]);
+server.route(require('./routes'));
 
 server.start((err) => {
     if(err) throw err
